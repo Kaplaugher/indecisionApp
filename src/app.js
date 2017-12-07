e6e144b5e104991c2b9d926747a9c70d51b0bc5b
Kaplaugher <kaplaugher94@gmail.com>
2017-12-07 12:34:17 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -5,15 +5,11 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
-    }
+      options: props.options
+    };
   }
   handleDeleteOptions() {
-    this.setState(() => {
-      return {
-        options: []
-      }
-    })
+    this.setState(() => ({ options: [] }));
   }
   handlePick() {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
@@ -22,87 +18,74 @@ class IndecisionApp extends React.Component {
   }
   handleAddOption(option) {
     if (!option) {
-      return 'Enter Valid Value'
-    }else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists';
+      return "Enter Valid Value";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
     }
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.concat([option])
-      }
-    });
+    this.setState(prevState => ({
+      options: prevState.options.concat([option])
+    }));
   }
   render() {
     const title = "indecision app";
     const subtitle = "Put your life in the hands of a computer";
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
-        <Action 
-        hasOptions={this.state.options.length > 0}
-        handlePick={this.handlePick}
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
         />
-        <Options 
-        options={this.state.options}
-        handleDeleteOptions={this.handleDeleteOptions} 
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
         />
-        <AddOption 
-        handleAddOption={this.handleAddOption}
-        />
+        <AddOption handleAddOption={this.handleAddOption} />
         <VisibilityToggle />
       </div>
     );
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
-}
+IndecisionApp.defaultProps = {
+  options: []
+};
 
-class Action extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        <button 
-        onClick={this.props.handlePick}
-        disabled={!this.props.hasOptions}>
+const Header = props => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
+};
+
+const Action = props => {
+  return (
+    <div>
+      <button onClick={props.handlePick} disabled={!props.hasOptions}>
         What should I do?
-        </button>
-      </div>
-    );
-  }
-}
+      </button>
+    </div>
+  );
+};
 
-// set up otions prop for options component
-// render length of the array
+const Options = props => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.map(option => <Option key={option} optionText={option} />)}
+    </div>
+  );
+};
 
-class Options extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        {this.props.options.map(option => (
-          <Option key={option} optionText={option} />
-        ))}
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    return <div>{this.props.optionText}</div>;
-  }
-}
+const Option = props => {
+  return <div>{props.optionText}</div>;
+};
 
 class AddOption extends React.Component {
   constructor(props) {
@@ -118,9 +101,7 @@ class AddOption extends React.Component {
     const option = event.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
 
-    this.setState(() => {
-      return { error };
-    })
+    this.setState(() => ({ error }));
   }
   render() {
     return (
@@ -141,14 +122,14 @@ class VisibilityToggle extends React.Component {
     this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
     this.state = {
       visibility: false
-    }
+    };
   }
   handleToggleVisibility() {
-    this.setState((prevState) => {
+    this.setState(prevState => {
       return {
         visibility: !prevState.visibility
-      }
-    })
+      };
+    });
   }
   render() {
     return (
@@ -166,5 +147,14 @@ class VisibilityToggle extends React.Component {
     );
   }
 }
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: </p>
+//     </div>
+//   );
+// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
